bb78fac6b3e5bcc4540d9061c6d4a9e6c8cbd637
Sebastian <sebastian@flyingpixel.com>
2023-04-13 22:39:58 +0200

    major minor changes

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ from flask import Flask, render_template
 # variables
 werbeligaURL = 'http://www.werbeliga.de/de/Spielplan,%20Tabelle%20&%20Torsch%C3%BCtzen' + \
     '?season=' + date.today().strftime('%y')
-current_matchday_value = ''
 matchday_values = []
 teams = []
 last_five_gameday_matchups = []
-current_table_html = ''
 
 # get current matchday value
 url = werbeligaURL
@@ -31,14 +29,12 @@ for value in matchday_values:
     soup = BeautifulSoup(currentMatchdayHTML.content, 'html.parser')
     # find first
     if soup.find_all('table')[0].find_all('tr')[1].find_all('td')[3].text.strip().split(' ')[0] != "-":
-        print('\n\n### Found current matchday! ###')
-        print('matchday_value: ' + current_matchday_value + '\n\n')
         break
 
 # get last 5 games
 for i in range(5):
-    response = requests.get(werbeligaURL + '&match=' +
-                            str(int(current_matchday_value) - i))
+    url = f"{werbeligaURL}&match={int(current_matchday_value) - i}"
+    response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
     if i == 0:
@@ -57,26 +53,27 @@ def getLast5Games(teamName):
             tds = row.find_all('td')
             if len(tds) != 0:
                 matchup = tds[2].text.strip()
-                if matchup and matchup.split(':')[0].strip() == teamName:
-                    score = tds[3].text.strip()
-                    if int(score.split(':')[0]) > int(score.split(':')[1]):
-                        last5.append('W')
-                    elif int(score.split(':')[0]) < int(score.split(':')[1]):
-                        last5.append('L')
-                    elif int(score.split(':')[0]) == int(score.split(':')[1]):
-                        last5.append('D')
-                elif matchup and matchup.split(':')[1].strip() == teamName:
-                    score = tds[3].text.strip()
-                    if int(score.split(':')[0]) > int(score.split(':')[1]):
-                        last5.append('L')
-                    elif int(score.split(':')[0]) < int(score.split(':')[1]):
-                        last5.append('W')
-                    elif int(score.split(':')[0]) == int(score.split(':')[1]):
-                        last5.append('D')
+                if matchup:
+                    home_goals, away_goal = tds[3].text.strip().split(':')
+                    home_goals, away_goal = int(home_goals), int(away_goal)
+                    if matchup.split(':')[0].strip() == teamName:
+                        if home_goals > away_goal:
+                            last5.append('W')
+                        elif home_goals < away_goal:
+                            last5.append('L')
+                        elif home_goals == away_goal:
+                            last5.append('D')
+                    elif matchup.split(':')[1].strip() == teamName:
+                        if home_goals > away_goal:
+                            last5.append('L')
+                        elif home_goals < away_goal:
+                            last5.append('W')
+                        elif home_goals == away_goal:
+                            last5.append('D')
     return last5
 
 
-tableString = ''
+table = []
 for row in current_table_html.find_all('tr'):
     rowStr = ''
     logoURL = ''
@@ -86,42 +83,24 @@ for row in current_table_html.find_all('tr'):
         else:
             logoURL = row.find('img')['src']
     # add row without last semicolon
-    tableString += rowStr + logoURL + '\n'
+    if rowStr != '':
+        table.append(rowStr + logoURL)
 
-tableString = tableString.rstrip()
-tableString = tableString.splitlines()
-tableString = [line for line in tableString if line]
 
-for line in tableString:
+for line in table:
     data = line.split(';')
-
-    position = data[0]
     name = data[1]
-    games = data[2]
-    wins = data[3]
-    draws = data[4]
-    losses = data[5]
-    goals = data[6].split(':')[0]
-    goalsAgainst = data[6].split(':')[1]
-    points = data[8]
-    logoURL = data[9]
-
     last5 = getLast5Games(name)
-
-    # new team
-    team = Team(name, points, goals, goalsAgainst, wins,
-                draws, losses, position, last5, logoURL)
-    # add team to list
+    team = Team(*data, last5)
     teams.append(team)
 
 
+# Flask
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
     return render_template('home.html', teams=teams)
-
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
--- team.py
+++ team.py
@@ -1,52 +1,34 @@
-# a class for a team
+import requests
+from PIL import Image
+from io import BytesIO
+from pathlib import Path
+
+def download_logo(url, name):
+    output_path = Path('static') / f'{name}.png'
+    if not output_path.exists():
+        response = requests.get(url)
+        img = Image.open(BytesIO(response.content))
+        output_path.parent.mkdir(exist_ok=True, parents=True)
+        img.save(output_path)
+    return f'{name}.png'
 
 class Team:
-    def __init__(self, name, points, goals, goalsAgainst, wins, draws, losses, position, last5, logoURL):
+    def __init__(self, position, name, games, wins, draws, losses, goals, goal_difference, points, logoURL, last5):
         self.name = name
         self.points = points
-        self.goals = goals
-        self.goalsAgainst = goalsAgainst
+        self.goals_scored = goals.split(':')[0]
+        self.goals_against = goals.split(':')[1]
+        self.games = games
         self.wins = wins
         self.draws = draws
         self.losses = losses
+        self.goal_difference = goal_difference
         self.position = position
         self.last5 = last5
-        self.logoURL = logoURL
+        self.logoURL = download_logo(logoURL, name)
 
     def __str__(self):
         return self.name
     
     def __repr__(self):
         return self.name + ' ' + self.points + ' ' + self.goals + ' ' + self.goalsAgainst + ' ' + self.wins + ' ' + self.draws + ' ' + self.losses + ' ' + self.place
-    
-    def getName(self):
-        return self.name
-    
-    def getPoints(self):
-        return self.points
-    
-    def getGoals(self):
-        return self.goals
-    
-    def getGoalsAgainst(self):
-        return self.goalsAgainst
-    
-    def getGoalDifference(self):
-        return int(self.goals) - int(self.goalsAgainst)
-    
-    def getWins(self):
-        return self.wins
-    
-    def getDraws(self):
-        return self.draws
-    
-    def getLosses(self):
-        return self.losses
-    
-    def getPlace(self):
-        return self.place
-    
-    def getNumberOfGames(self):
-        return int(self.wins) + int(self.draws) + int(self.losses)
-    
-
